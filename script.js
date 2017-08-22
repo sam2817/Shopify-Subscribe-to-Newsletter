8bdc95e8d064bc6def403c0c4f69ebfebb1921e2
sam2817 <sam2817@users.noreply.github.com>
2017-08-22 11:59:09 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function ValidateEmail(inputText)  
+{  
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;  
+if(inputText.value.match(mailformat))  
+{  
+document.subscribe.form.email.focus();  
+return true;  
+}  
+else  
+{  
+alert("You have entered an invalid email address!");  
+document.subscribe.form.email.focus();  
+return false;  
+}  
+}  
